d94c47091ecbbbd12aa6ae5f365336bf07c769e4
richie mbengui <richie.mbengui@rakuten.com>
2022-12-05 07:54:34 +0100

    improve css

--- src/components/Card/__tests__/card.test.jsx
+++ src/components/Card/__tests__/card.test.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Card } from '../Card';
@@ -7,6 +6,6 @@ test('show data in component card', () => {
     render(<Card day={{tmin:4,tmax:5, datetime: new Date().toDateString(),sun_hours:3 }} />);
     
     expect(screen.getByText(new Date().toDateString())).toBeVisible();
-    expect(screen.getByText('Température minimum : 4 °')).toBeVisible();
-    expect(screen.getByText('Température maximum : 5 °')).toBeVisible();
+    expect(screen.getByText('4°C')).toBeVisible();
+    expect(screen.getByText('5°C')).toBeVisible();
 });  
